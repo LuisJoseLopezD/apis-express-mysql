6da89637737b275ee939c0709841fd4b744e606b
Luis José López Delgado <luisjoselopezd@gmail.com>
2023-09-02 23:49:46 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    db: {
+      
+      host: "db4free.net",
+      user: "luis_20",
+      password: "#.pa!HKRM86bX@n",
+      database: "testmysql10",
+      connectTimeout: 60000
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
